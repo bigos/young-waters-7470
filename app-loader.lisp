ced96022e3116f98738372b0513b012c1bbc7469
Jacek <ruby.object@googlemail.com>
2013-02-14 01:13:19 +0000

    added the app loader

---
+++ app-loader.lisp
@@ -0,0 +1,6 @@
+(progn 
+(ql:quickload :caveman) 
+(ql:quickload :quickproject) 
+(quickproject:make-project "/app/flintstone/" :depends-on '(caveman)))
+(ql:quickload :flintstone)
+(flintstone:start)
