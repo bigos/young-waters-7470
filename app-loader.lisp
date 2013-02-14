1d69690b6c778fa328501def21dad0c155d24820
Jacek <ruby.object@googlemail.com>
2013-02-14 02:18:21 +0000

    trying to use asdf 20

--- app-loader.lisp
+++ app-loader.lisp
@@ -1,11 +1,8 @@
  
 (require 'asdf)
 (asdf:disable-output-translations)
-(require-quicklisp)
 
-(ql:quickload :caveman) 
 (push #p"/app/flintstone/" asdf:*central-registry*)
 (asdf:operate 'asdf:load-op :flintstone)
-;(ql:quickload :flintstone)
 (flintstone:start)
 
