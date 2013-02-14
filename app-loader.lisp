d7438d76bed9b31fe97c7c04e9dfd9e991c67149
Jacek <ruby.object@googlemail.com>
2013-02-14 01:33:59 +0000

    trying to use asdf 13

--- app-loader.lisp
+++ app-loader.lisp
@@ -1,6 +1,6 @@
 (progn 
 (ql:quickload :caveman) 
-(push #p"flintstone/" asdf:*central-registry*)
-(format t "~a~%" asdf:*central-registry*)
+(push #p"/app/flintstone/" asdf:*central-registry*)
+(format t "::::::: ~s~%" asdf:*central-registry*)
 (ql:quickload :flintstone)
 (flintstone:start))
