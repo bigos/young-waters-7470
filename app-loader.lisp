cf39371518de9deaaa3f0a2de21c3feafc7ac773
Jacek Podkanski <ruby.object@googlemail.com>
2013-02-18 12:51:21 +0000

    trying loader without specifying port

--- app-loader.lisp
+++ app-loader.lisp
@@ -4,4 +4,4 @@
 
 (push #p"/app/flintstone/" asdf:*central-registry*)
 (asdf:operate 'asdf:load-op :flintstone)
-(flintstone:start   :port (or (parse-integer (format nil "~a" (sb-ext:posix-getenv "PORT"))) 5000))
+(flintstone:start :port (parse-integer (sb-ext:posix-getenv "PORT")))
